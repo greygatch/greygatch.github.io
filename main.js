91d1b183e846f85218fc2f51b9dd588df8e65d44
Evan Gatchell <egatchell@changehealthcare.com>
2017-01-25 10:24:15 -0600

    reinitializing git

---
+++ main.js
@@ -0,0 +1,15 @@
+$(document).ready(init);
+
+function printConsoleMessage(){
+  console.log('Hello, world!!!!')
+}
+
+function init(){
+  $('#fullpage').fullpage();
+  
+  $('.hidden').hide();
+  $('.hidden').fadeIn(3000);
+
+  printConsoleMessage();
+
+}
